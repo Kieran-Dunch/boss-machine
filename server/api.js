78964a53a4dfcb67c588c5a41868bcc8668191ed
Kieran Dunch <dunch.kieran@gmail.com>
2024-06-03 09:07:40 -0600

    rework og file

--- server/api.js
+++ server/api.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-// routes required:
-
-// /api/minions
 const minionsRouter = require('./minions');
-
-app.use('/api/minions', minionsRouter);
-// GET /api/minions to get an array of all minions.
-// POST /api/minions to create a new minion and save it to the database.
-// GET /api/minions/:minionId to get a single minion by id.
-// PUT /api/minions/:minionId to update a single minion by id.
-// DELETE /api/minions/:minionId to delete a single minion by id.
 const ideasRouter = require('./ideas');
+const meetingsRouter = require('./meetings');
 
-app.use('api/ideas', ideasRouter);
-// GET /api/ideas to get an array of all ideas.
-// POST /api/ideas to create a new idea and save it to the database.
-// GET /api/ideas/:ideaId to get a single idea by id.
-// PUT /api/ideas/:ideaId to update a single idea by id.
-// DELETE /api/ideas/:ideaId to delete a single idea by id.
-
-app.use('api/meetings', meetingsRouter);
-// /api/meetings
-// GET /api/meetings to get an array of all meetings.
-// POST /api/meetings to create a new meeting and save it to the database.
-// DELETE /api/meetings to delete all meetings from the database.
+apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
