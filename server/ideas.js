955465b4aff7f5b17a48761938b091d34f4af9f7
Kieran Dunch <dunch.kieran@gmail.com>
2024-06-05 09:21:02 -0600

    ideas routes

--- server/ideas.js
+++ server/ideas.js
@@ -2,4 +2,51 @@ const express = require('express');
 
 const ideasRouter = express.Router();
 
+const {
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require('./db');
+
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  res.send(req.idea);
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+  const updatedIdeaInstance = updateInstanceInDatabase('ideas', req.body);
+  res.send(updatedIdeaInstance);
+});
+
+ideasRouter.post('/', (req, res, next) => {
+  const newIdea = addToDatabase('ideas', req.body);
+  res.status(201).send(newIdea);
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  const deleted = deleteFromDatabasebyId('ideas', req.params.ideaId);
+  if (deleted) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+});
+
 module.exports = ideasRouter;
