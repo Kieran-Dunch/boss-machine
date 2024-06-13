3b9090a848abf742f95057938cc176a9331f703f
Kieran Dunch <dunch.kieran@gmail.com>
2024-06-13 13:34:35 -0600

    meetings routes

--- server/meetings.js
+++ server/meetings.js
@@ -2,4 +2,36 @@ const express = require('express');
 
 const meetingsRouter = express.Router();
 
+const {
+  createMeeting,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  deleteAllFromDatabase,
+} = require('./db');
+
+meetingsRouter.param('meetingId', (req, res, next, id) => {
+  const meeting = getFromDatabaseById('meetings', id);
+  if (meeting) {
+    req.meeting = meeting;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+meetingsRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('meetings'));
+});
+
+meetingsRouter.post('/', (req, res, next) => {
+  const newMeeting = addToDatabase('meetings', createMeeting());
+  res.status(201).send(newMeeting);
+});
+
+meetingsRouter.delete('/', (req, res, next) => {
+  deleteAllFromDatabase('meetings');
+  res.status(204).send();
+});
+
 module.exports = meetingsRouter;
